e03c4e354c231caf30510af280ba4703945c796c
nOOb3167 <nOOb3167@gmail.com>
2017-06-13 11:59:35 +0200

    psflsl: cmake fixing MavenInstall

--- cmake/BuildHelper/MavenInstallHelper.cmake
+++ cmake/BuildHelper/MavenInstallHelper.cmake
@@ -1,56 +1,48 @@
-# check required variables were defined when executing this file
-
-IF (NOT CMAKE_INSTALL_PREFIX)
-  MESSAGE(FATAL_ERROR "MavenInstallHelper Missing CMAKE_INSTALL_PREFIX")
+IF (NOT CMAKE_INSTALL_PREFIX OR NOT PSFLSL_MAVEN_COMMAND OR NOT PSFLSL_FILE_DIR OR NOT PSFLSL_FILE_NAME_32 OR NOT PSFLSL_FILE_NAME_64)
+  MESSAGE(FATAL_ERROR "May have been invoked without setting the necessary variables")
 ENDIF ()
 
-IF (NOT PSFLSL_MAVEN_COMMAND)
-  MESSAGE(FATAL_ERROR "MavenInstallHelper Missing PSFLSL_MAVEN_COMMAND")
+GET_FILENAME_COMPONENT(PSFLSL_ARTIFACTID_32 "${PSFLSL_FILE_NAME_32}" NAME_WE)
+GET_FILENAME_COMPONENT(PSFLSL_ARTIFACTID_64 "${PSFLSL_FILE_NAME_64}" NAME_WE)
+
+SET(PSFLSL_FILE_32 "${PSFLSL_FILE_DIR}/${PSFLSL_FILE_NAME_32}")
+SET(PSFLSL_FILE_64 "${PSFLSL_FILE_DIR}/${PSFLSL_FILE_NAME_64}")
+
+IF (NOT (EXISTS "${PSFLSL_FILE_32}") OR NOT (EXISTS "${PSFLSL_FILE_64}"))
+  MESSAGE(FATAL_ERROR "32 or 64 bit Launcher executable not found"
+                       "- an install may not have successfully run")
 ENDIF ()
-
-# check required target export files are present
-
-SET(PSFLSL_EXPORTED_FILENAME_32 "${CMAKE_INSTALL_PREFIX}/cmake/x32-psflsl-executable-export.cmake")
-SET(PSFLSL_EXPORTED_FILENAME_64 "${CMAKE_INSTALL_PREFIX}/cmake/x64-psflsl-executable-export.cmake")
-
-IF (NOT (EXISTS "${PSFLSL_EXPORTED_FILENAME_32}"))
-  MESSAGE(FATAL_ERROR "MavenInstallHelper Missing File (PSFLSL_EXPORTED_FILENAME_32)")
-ENDIF ()
-
-IF (NOT (EXISTS "${PSFLSL_EXPORTED_FILENAME_64}"))
-  MESSAGE(FATAL_ERROR "MavenInstallHelper Missing File (PSFLSL_EXPORTED_FILENAME_64)")
-ENDIF ()
-
-# import target export files
-
-INCLUDE("${PSFLSL_EXPORTED_FILENAME_32}")
-INCLUDE("${PSFLSL_EXPORTED_FILENAME_64}")
-
-# run maven install plugin
-
-## artifactId are the executable filenames without extension
-
-SET(PSFLSL_FILE_32 "$<TARGET_FILE_NAME:x32-PSF-LoginServer-Launcher>")
-SET(PSFLSL_FILE_64 "$<TARGET_FILE_NAME:x64-PSF-LoginServer-Launcher>")
-
-GET_FILENAME_COMPONENT(PSFLSL_ARTIFACTID_32 "${PSFLSL_FILE_32}" NAME_WE)
-GET_FILENAME_COMPONENT(PSFLSL_ARTIFACTID_64 "${PSFLSL_FILE_64}" NAME_WE)
 
 EXECUTE_PROCESS(
   COMMAND
     "${PSFLSL_MAVEN_COMMAND}"
     "org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file"
-    -Dfile="${PSFLSL_FILE_32}"
-    -DgroupId="net.psforever.launcher"
-    -DartifactId="${PSFLSL_ARTIFACTID_32}"
-    -Dversion="0.1"
-    -Dpackaging="exe"
+    "-Dfile=${PSFLSL_FILE_32}"
+    "-DgroupId=net.psforever.launcher"
+    "-DartifactId=${PSFLSL_ARTIFACTID_32}"
+    "-Dversion=NOVERSION"
+    "-Dpackaging=exe"
   COMMAND
     "${PSFLSL_MAVEN_COMMAND}"
     "org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file"
-    -Dfile="${PSFLSL_FILE_64}"
-    -DgroupId="net.psforever.launcher"
-    -DartifactId="${PSFLSL_ARTIFACTID_64}"
-    -Dversion="0.1"
-    -Dpackaging="exe"
+    "-Dfile=${PSFLSL_FILE_64}"
+    "-DgroupId=net.psforever.launcher"
+    "-DartifactId=${PSFLSL_ARTIFACTID_64}"
+    "-Dversion=NOVERSION"
+    "-Dpackaging=exe"
+  RESULT_VARIABLE
+    TMP_RETCODE
+  OUTPUT_VARIABLE
+    TMP_STREAM
+  ERROR_VARIABLE
+    TMP_STREAM
 )
+
+IF (NOT "${TMP_RETCODE}" STREQUAL "0")
+  MESSAGE("Maven Executable (NOTE: may need full path!): ${PSFLSL_MAVEN_COMMAND}")
+  MESSAGE("Error Code: ${TMP_RETCODE}")
+  MESSAGE("(BEGIN MAVEN STDOUT STDERR DUMP)")
+  MESSAGE("${TMP_STREAM}")
+  MESSAGE("(END   MAVEN STDOUT STDERR DUMP)")
+  MESSAGE(FATAL_ERROR "Error Invoking Maven")
+ENDIF ()
